92d4867bb44492bb7f1f4266ff9c2c1ab099b0eb
Swen E Gaudl <swen.gaudl@gmail.com>
2015-09-25 15:46:28 +0100

    included simple XML for later on loading chromosomes from file tweaked the TournamentSelector to output the stats to Console

--- src/org/jgap/gp/GPProgramBase.java
+++ src/org/jgap/gp/GPProgramBase.java
@@ -11,7 +11,10 @@ package org.jgap.gp;
 
 import org.jgap.*;
 import org.jgap.gp.impl.*;
+import org.simpleframework.xml.Default;
+import org.simpleframework.xml.Root;
 
+@Default
 /**
  * Base class for GPProgram's. See org.jgap.gp.impl.GPProgram for an
  * implementation.
@@ -20,256 +23,257 @@ import org.jgap.gp.impl.*;
  * @since 3.0
  */
 public abstract class GPProgramBase
-    implements IGPProgram {
-  /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.14 $";
+implements IGPProgram {
+	
+	/** String containing the CVS revision. Read out via reflection!*/
+	private final static String CVS_REVISION = "$Revision: 1.14 $";
 
-  private double m_fitnessValue = FitnessFunction.NO_FITNESS_VALUE;
+	private double m_fitnessValue = FitnessFunction.NO_FITNESS_VALUE;
 
-  private GPConfiguration m_conf;
-  
-  private boolean reEvaluate;
-  
-  /**
-   * The scaling factor is used for the parsimony pressure to rescale the fitness value based 
-   * on its current performance within the generation. By default the value is 1 ad after cloning it is also reset to 1
-   */
-  private double m_scalingFactor = 1d;
+	private GPConfiguration m_conf;
+
+	private boolean reEvaluate;
+
+	/**
+	 * The scaling factor is used for the parsimony pressure to rescale the fitness value based 
+	 * on its current performance within the generation. By default the value is 1 ad after cloning it is also reset to 1
+	 */
+	private double m_scalingFactor = 1d;
 
 
-  /**
-   * Return type per chromosome.
-   */
-  private Class[] m_types;
+	/**
+	 * Return type per chromosome.
+	 */
+	private Class[] m_types;
 
-  /**
-   * Argument types for ADF's
-   */
-  private Class[][] m_argTypes;
+	/**
+	 * Argument types for ADF's
+	 */
+	private Class[][] m_argTypes;
 
-  /**
-   * Available GP-functions.
-   */
-  private CommandGene[][] m_nodeSets;
+	/**
+	 * Available GP-functions.
+	 */
+	private CommandGene[][] m_nodeSets;
 
-  /**
-   * Minimum depth per each chromosome
-   */
-  private int[] m_minDepths;
+	/**
+	 * Minimum depth per each chromosome
+	 */
+	private int[] m_minDepths;
 
-  /**
-   * Maximum depth per each chromosome
-   */
-  private int[] m_maxDepths;
+	/**
+	 * Maximum depth per each chromosome
+	 */
+	private int[] m_maxDepths;
 
-  /**
-   * Maximum number of nodes allowed per chromosome (when exceeded program
-   * aborts)
-   */
-  private int m_maxNodes;
+	/**
+	 * Maximum number of nodes allowed per chromosome (when exceeded program
+	 * aborts)
+	 */
+	private int m_maxNodes;
 
-  /**
-   * Free to use data object.
-   */
-  private Object m_applicationData;
+	/**
+	 * Free to use data object.
+	 */
+	private Object m_applicationData;
 
-  /**
-   * Default constructor, only for dynamic instantiation.
-   *
-   * @throws Exception
-   *
-   * @author Klaus Meffert
-   * @since 3.3.4
-   */
-  public GPProgramBase()
-      throws Exception {
-  }
+	/**
+	 * Default constructor, only for dynamic instantiation.
+	 *
+	 * @throws Exception
+	 *
+	 * @author Klaus Meffert
+	 * @since 3.3.4
+	 */
+	public GPProgramBase()
+			throws Exception {
+	}
 
-  public GPProgramBase(GPConfiguration a_conf)
-      throws InvalidConfigurationException {
-    if (a_conf == null) {
-      throw new InvalidConfigurationException("Configuration must not be null!");
-    }
-    m_conf = a_conf;
-  }
+	public GPProgramBase(GPConfiguration a_conf)
+			throws InvalidConfigurationException {
+		if (a_conf == null) {
+			throw new InvalidConfigurationException("Configuration must not be null!");
+		}
+		m_conf = a_conf;
+	}
 
-  public GPProgramBase(IGPProgram a_prog)
-      throws InvalidConfigurationException {
-    this(a_prog.getGPConfiguration());
-    m_types = a_prog.getTypes();
-    m_argTypes = a_prog.getArgTypes();
-    m_nodeSets = a_prog.getNodeSets();
-    m_maxDepths = a_prog.getMaxDepths();
-    m_minDepths = a_prog.getMinDepths();
-    m_maxNodes = a_prog.getMaxNodes();
-  }
+	public GPProgramBase(IGPProgram a_prog)
+			throws InvalidConfigurationException {
+		this(a_prog.getGPConfiguration());
+		m_types = a_prog.getTypes();
+		m_argTypes = a_prog.getArgTypes();
+		m_nodeSets = a_prog.getNodeSets();
+		m_maxDepths = a_prog.getMaxDepths();
+		m_minDepths = a_prog.getMinDepths();
+		m_maxNodes = a_prog.getMaxNodes();
+	}
 
-  public GPConfiguration getGPConfiguration() {
-    return m_conf;
-  }
+	public GPConfiguration getGPConfiguration() {
+		return m_conf;
+	}
 
-  /**
-   * Compares this entity against the specified object.
-   *
-   * @param a_other the object to compare against
-   * @return true: if the objects are the same, false otherwise
-   *
-   * @author Klaus Meffert
-   * @since 3.0
-   */
-  public boolean equals(Object a_other) {
-    try {
-      return compareTo(a_other) == 0;
-    } catch (ClassCastException cex) {
-      return false;
-    }
-  }
+	/**
+	 * Compares this entity against the specified object.
+	 *
+	 * @param a_other the object to compare against
+	 * @return true: if the objects are the same, false otherwise
+	 *
+	 * @author Klaus Meffert
+	 * @since 3.0
+	 */
+	public boolean equals(Object a_other) {
+		try {
+			return compareTo(a_other) == 0;
+		} catch (ClassCastException cex) {
+			return false;
+		}
+	}
 
-  /**
-   * @return fitness value of this program determined via the registered
-   * fitness function
-   *
-   * @author Klaus Meffert
-   * @since 3.0
-   */
-  public double calcFitnessValue() {
-    GPFitnessFunction normalFitnessFunction = getGPConfiguration().
-        getGPFitnessFunction();
-    if (normalFitnessFunction != null) {
-      // Grab the "normal" fitness function and ask it to calculate our
-      // fitness value.
-      // --------------------------------------------------------------
-      m_fitnessValue = normalFitnessFunction.getFitnessValue(this);
-    }
-    if (Double.isInfinite(m_fitnessValue)) {
-      return GPFitnessFunction.NO_FITNESS_VALUE;
-    }
-    else {
-      return m_fitnessValue;
-    }
-  }
+	/**
+	 * @return fitness value of this program determined via the registered
+	 * fitness function
+	 *
+	 * @author Klaus Meffert
+	 * @since 3.0
+	 */
+	public double calcFitnessValue() {
+		GPFitnessFunction normalFitnessFunction = getGPConfiguration().
+				getGPFitnessFunction();
+		if (normalFitnessFunction != null) {
+			// Grab the "normal" fitness function and ask it to calculate our
+			// fitness value.
+			// --------------------------------------------------------------
+			m_fitnessValue = normalFitnessFunction.getFitnessValue(this);
+		}
+		if (Double.isInfinite(m_fitnessValue)) {
+			return GPFitnessFunction.NO_FITNESS_VALUE;
+		}
+		else {
+			return m_fitnessValue;
+		}
+	}
 
-  /**
-   * @return fitness value of this program, cached access
-   *
-   * @author Klaus Meffert
-   * @since 3.0
-   */
-  public double getFitnessValue() {
-    if (m_fitnessValue >= 0.000d && !reEvaluate) {
-      return m_scalingFactor*m_fitnessValue;
-    }
-    else {
-      return m_scalingFactor * calcFitnessValue();
-    }
-  }
+	/**
+	 * @return fitness value of this program, cached access
+	 *
+	 * @author Klaus Meffert
+	 * @since 3.0
+	 */
+	public double getFitnessValue() {
+		if (m_fitnessValue >= 0.000d && !reEvaluate) {
+			return m_scalingFactor*m_fitnessValue;
+		}
+		else {
+			return m_scalingFactor * calcFitnessValue();
+		}
+	}
 
-  /**
-   * @return computed fitness value of this program, may be unitialized
-   *
-   * @author Klaus Meffert
-   * @since 3.2
-   */
-  public double getFitnessValueDirectly() {
-    return m_fitnessValue;
-  }
+	/**
+	 * @return computed fitness value of this program, may be unitialized
+	 *
+	 * @author Klaus Meffert
+	 * @since 3.2
+	 */
+	public double getFitnessValueDirectly() {
+		return m_fitnessValue;
+	}
 
-  public void setFitnessValue(double a_fitness) {
-    m_fitnessValue = a_fitness;
-  }
-  
-  @Override
-  public void setScalingFactor(double scale) {
-	m_scalingFactor = scale;
-  }
+	public void setFitnessValue(double a_fitness) {
+		m_fitnessValue = a_fitness;
+	}
 
-  public void setTypes(Class[] a_types) {
-    m_types = a_types;
-  }
+	@Override
+	public void setScalingFactor(double scale) {
+		m_scalingFactor = scale;
+	}
 
-  public Class[] getTypes() {
-    return m_types;
-  }
+	public void setTypes(Class[] a_types) {
+		m_types = a_types;
+	}
 
-  public Class getType(int a_index) {
-    return m_types[a_index];
-  }
+	public Class[] getTypes() {
+		return m_types;
+	}
 
-  public void setArgTypes(Class[][] a_argTypes) {
-    m_argTypes = a_argTypes;
-  }
+	public Class getType(int a_index) {
+		return m_types[a_index];
+	}
 
-  public Class[][] getArgTypes() {
-    return m_argTypes;
-  }
+	public void setArgTypes(Class[][] a_argTypes) {
+		m_argTypes = a_argTypes;
+	}
 
-  public Class[] getArgType(int a_index) {
-    return m_argTypes[a_index];
-  }
+	public Class[][] getArgTypes() {
+		return m_argTypes;
+	}
 
-  public void setNodeSets(CommandGene[][] a_nodeSets) {
-    m_nodeSets = a_nodeSets;
-  }
+	public Class[] getArgType(int a_index) {
+		return m_argTypes[a_index];
+	}
 
-  public CommandGene[][] getNodeSets() {
-    return m_nodeSets;
-  }
+	public void setNodeSets(CommandGene[][] a_nodeSets) {
+		m_nodeSets = a_nodeSets;
+	}
 
-  public CommandGene[] getNodeSet(int a_index) {
-    return m_nodeSets[a_index];
-  }
+	public CommandGene[][] getNodeSets() {
+		return m_nodeSets;
+	}
 
-  public void setMaxDepths(int[] a_maxDepths) {
-    m_maxDepths = a_maxDepths;
-  }
+	public CommandGene[] getNodeSet(int a_index) {
+		return m_nodeSets[a_index];
+	}
 
-  public int[] getMaxDepths() {
-    return m_maxDepths;
-  }
+	public void setMaxDepths(int[] a_maxDepths) {
+		m_maxDepths = a_maxDepths;
+	}
 
-  public void setMinDepths(int[] a_minDepths) {
-    m_minDepths = a_minDepths;
-  }
+	public int[] getMaxDepths() {
+		return m_maxDepths;
+	}
 
-  public int[] getMinDepths() {
-    return m_minDepths;
-  }
+	public void setMinDepths(int[] a_minDepths) {
+		m_minDepths = a_minDepths;
+	}
 
-  public void setMaxNodes(int a_maxNodes) {
-    m_maxNodes = a_maxNodes;
-  }
+	public int[] getMinDepths() {
+		return m_minDepths;
+	}
 
-  public int getMaxNodes() {
-    return m_maxNodes;
-  }
+	public void setMaxNodes(int a_maxNodes) {
+		m_maxNodes = a_maxNodes;
+	}
 
-  /**
-   * Sets the application data object.
-   *
-   * @param a_data the object to set
-   *
-   * @author Klaus Meffert
-   * @since 3.01
-   */
-  public void setApplicationData(Object a_data) {
-    m_applicationData = a_data;
-  }
+	public int getMaxNodes() {
+		return m_maxNodes;
+	}
 
-  /**
-   * @return the application data object set
-   *
-   * @author Klaus Meffert
-   * @since 3.01
-   */
-  public Object getApplicationData() {
-    return m_applicationData;
-  }
+	/**
+	 * Sets the application data object.
+	 *
+	 * @param a_data the object to set
+	 *
+	 * @author Klaus Meffert
+	 * @since 3.01
+	 */
+	public void setApplicationData(Object a_data) {
+		m_applicationData = a_data;
+	}
 
-  /**
-   * @return deep clone of this instance
-   *
-   * @author Klaus Meffert
-   * @since 3.2
-   */
-  public abstract Object clone();
+	/**
+	 * @return the application data object set
+	 *
+	 * @author Klaus Meffert
+	 * @since 3.01
+	 */
+	public Object getApplicationData() {
+		return m_applicationData;
+	}
+
+	/**
+	 * @return deep clone of this instance
+	 *
+	 * @author Klaus Meffert
+	 * @since 3.2
+	 */
+	public abstract Object clone();
 }
